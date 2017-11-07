7ed6a24550085d9a97e221dc82c62a0724993951
John Knotts <jhnnyk@gmail.com>
2017-11-07 11:01:13 -0700

    add first test 🎉

---
+++ src/components/game.test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {shallow, mount} from 'enzyme'
+
+import Game from './game'
+
+describe('<Game />', () => {
+  it('renders without crashing', () => {
+    shallow(<Game />)
+  })
+})
