a2f24565e0851c880248750080bd8444c0c81b21
John Knotts <jhnnyk@gmail.com>
2017-11-07 12:10:32 -0700

    add smoke test for each component

---
+++ src/components/guess-list.test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { shallow, mount } from 'enzyme'
+
+import GuessList from './guess-list'
+
+describe('<GuessList />', () => {
+  it('renders without crashing', () => {
+    shallow(<GuessList guesses={['34', '45']}/>)
+  })
+})
